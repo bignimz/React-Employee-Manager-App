1cbe0f6907cc7f204acb1625f7280337267af377
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-10-06 22:53:06 +0300

    created Employees component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import "./App.css";
+import Employees from "./Employees";
+import { useState } from "react";
+
+// Craete an array of employees
+// This can be replaced with an API data
+const employees = [
+  { Name: "John", Email: "john@example.com", Role: "Developer" },
+  { Name: "Mary", Email: "Mary@example.com", Role: "Intern" },
+  { Name: "Jack", Email: "jack@example.com", Role: "Senior Developer" },
+  { Name: "Brian", Email: "brian@example.com", Role: "Junior Developer" },
+  { Name: "Andrew", Email: "Andrew@example.com", Role: "Lead Developer" },
+];
+const ShowEmployees = true;
 
 function App() {
-  return <div></div>;
+  // useState Hook
+  const [role, setRole] = useState("dev");
+  return (
+    <div>
+      {ShowEmployees ? (
+        <>
+          <input
+            type="text"
+            placeholder="Enter role"
+            onChange={(e) => {
+              setRole(e.target.value);
+            }}
+          />
+          <Employees name="Brian" role={role} />
+        </>
+      ) : (
+        <h4>No Employees Available</h4>
+      )}
+    </div>
+  );
 }
 
 export default App;
